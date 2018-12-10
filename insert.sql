6f9e31dd55bd15376e9d1469db65ea5719d63d27
DESKTOP-QUPUQ2M\Minja <mrcha993@gmail.com>
2018-12-10 16:22:56 +0100

    Dodati upiti u opis domena i trigeri

--- insert.sql
+++ insert.sql
@@ -1,7 +1,48 @@
-/*
--- Query: 
--- Date: 2018-12-08 19:14
-*/
-INSERT INTO `Trening` (`id`,`Datum`,`Opis`) VALUES (1,'1.1.2018.','...');
-INSERT INTO `Trening` (`id`,`Datum`,`Opis`) VALUES (NULL,'2.1.2018.','asdasd');
-INSERT INTO `Trening` (`id`,`Datum`,`Opis`) VALUES (NULL,'21.1.2018.','asddsa');
+INSERT INTO `crossfit`.`Trening` (`id`, `Datum`, `Opis`) VALUES (1, '2018-11-11', '100 sklekova');
+INSERT INTO `crossfit`.`Trening` (`id`, `Datum`, `Opis`) VALUES (2, '2018-12-12', '100 trbusnjaka');
+INSERT INTO `crossfit`.`Trening` (`id`, `Datum`, `Opis`) VALUES (3, '2018-12-15', '100 cucnjeva');
+INSERT INTO `crossfit`.`Trening` (`id`, `Datum`, `Opis`) VALUES (4, '2018-12-20', '100 LUS-eva');
+
+INSERT INTO `crossfit`.`Trener` (`id`, `Ime`, `Prezime`, `Plata`, `DatumRodjenja`) VALUES (1, 'Sreten', 'Bogicevic', 60000, '1973-06-28');
+INSERT INTO `crossfit`.`Trener` (`id`, `Ime`, `Prezime`, `Plata`, `DatumRodjenja`) VALUES (2, 'Nikola', 'Tatalovic', 55000, '1990-02-06');
+INSERT INTO `crossfit`.`Trener` (`id`, `Ime`, `Prezime`, `Plata`, `DatumRodjenja`) VALUES (3, 'Marina', 'Petkovic', 30000, '1992-11-05');
+
+INSERT INTO `crossfit`.`Vodi` (`Trener_id`, `Trening_id`) VALUES (1, 1);
+INSERT INTO `crossfit`.`Vodi` (`Trener_id`, `Trening_id`) VALUES (2, 2);
+INSERT INTO `crossfit`.`Vodi` (`Trener_id`, `Trening_id`) VALUES (3, 3);
+INSERT INTO `crossfit`.`Vodi` (`Trener_id`, `Trening_id`) VALUES (1, 4);
+
+INSERT INTO `crossfit`.`Klijent` (`id`, `Ime`, `Prezime`, `BrojOdradjenihTreninga`) VALUES (1, 'Pera', 'Peric', 7);
+INSERT INTO `crossfit`.`Klijent` (`id`, `Ime`, `Prezime`, `BrojOdradjenihTreninga`) VALUES (2, 'Ivan', 'Ivanovic', 3);
+INSERT INTO `crossfit`.`Klijent` (`id`, `Ime`, `Prezime`, `BrojOdradjenihTreninga`) VALUES (3, 'Mika', 'Mikic', 14);
+
+INSERT INTO `crossfit`.`Prijava` (`Trening_id`, `Klijent_id`) VALUES (1, 1);
+INSERT INTO `crossfit`.`Prijava` (`Trening_id`, `Klijent_id`) VALUES (1, 2);
+INSERT INTO `crossfit`.`Prijava` (`Trening_id`, `Klijent_id`) VALUES (2, 3);
+INSERT INTO `crossfit`.`Prijava` (`Trening_id`, `Klijent_id`) VALUES (2, 2);
+INSERT INTO `crossfit`.`Prijava` (`Trening_id`, `Klijent_id`) VALUES (3, 1);
+INSERT INTO `crossfit`.`Prijava` (`Trening_id`, `Klijent_id`) VALUES (3, 3);
+INSERT INTO `crossfit`.`Prijava` (`Trening_id`, `Klijent_id`) VALUES (4, 1);
+
+INSERT INTO `crossfit`.`Oprema` (`sifra`, `Proizvodjac`, `Naziv`) VALUES (1, 'Nike', 'Lopte');
+INSERT INTO `crossfit`.`Oprema` (`sifra`, `Proizvodjac`, `Naziv`) VALUES (2, 'Reebok', 'Vijace');
+INSERT INTO `crossfit`.`Oprema` (`sifra`, `Proizvodjac`, `Naziv`) VALUES (3, 'Under Armor', 'Tegovi');
+INSERT INTO `crossfit`.`Oprema` (`sifra`, `Proizvodjac`, `Naziv`) VALUES (4, 'Addidas', 'Razgibavanje');
+INSERT INTO `crossfit`.`Oprema` (`sifra`, `Proizvodjac`, `Naziv`) VALUES (5, 'Addidas', 'Tegovi');
+INSERT INTO `crossfit`.`Oprema` (`sifra`, `Proizvodjac`, `Naziv`) VALUES (6, 'Nike', 'Tegovi');
+INSERT INTO `crossfit`.`Oprema` (`sifra`, `Proizvodjac`, `Naziv`) VALUES (7, 'Under Armor', 'Lopte');
+
+INSERT INTO `crossfit`.`Tegovi` (`Oprema_sifra`, `Tezina`, `Kolicina`, `Opis`) VALUES (3, 17, 3, 'bucice');
+INSERT INTO `crossfit`.`Tegovi` (`Oprema_sifra`, `Tezina`, `Kolicina`, `Opis`) VALUES (5, 25, 5, 'ploce');
+INSERT INTO `crossfit`.`Tegovi` (`Oprema_sifra`, `Tezina`, `Kolicina`, `Opis`) VALUES (6, 12, 10, 'kugle');
+
+INSERT INTO `crossfit`.`Medicinke` (`Oprema_sifra`, `Tezina`, `Kolicina`) VALUES (1, 9, 10);
+INSERT INTO `crossfit`.`Medicinke` (`Oprema_sifra`, `Tezina`, `Kolicina`) VALUES (7, 6, 5);
+
+INSERT INTO `crossfit`.`Strunjace` (`Oprema_sifra`, `Kolicina`) VALUES (4, 25);
+
+INSERT INTO `crossfit`.`Upotreba` (`Vodi_Trener_id`, `Vodi_Trening_id`, `Oprema_sifra`) VALUES (1, 4, 5);
+INSERT INTO `crossfit`.`Upotreba` (`Vodi_Trener_id`, `Vodi_Trening_id`, `Oprema_sifra`) VALUES (2, 2, 7);
+
+INSERT INTO `crossfit`.`Obucavanje` (`Trener_id`, `Trener_id1`) VALUES (1, 2);
+INSERT INTO `crossfit`.`Obucavanje` (`Trener_id`, `Trener_id1`) VALUES (1, 3);
--- script.sql
+++ script.sql
@@ -10,11 +10,47 @@ USE `crossfit`;
 -- -----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `crossfit`.`Klijent` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL,
   `Ime` VARCHAR(45) NOT NULL,
   `Prezime` VARCHAR(45) NOT NULL,
-  `BrojOdradjenihTreninga` INT ZEROFILL NULL,
+  `BrojOdradjenihTreninga` INT NULL DEFAULT 0,
   PRIMARY KEY (`id`));
+
+
+-- -----------------------------------------------------
+-- Table `crossfit`.`Trener`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `crossfit`.`Trener` (
+  `id` INT NOT NULL,
+  `Ime` VARCHAR(45) NOT NULL,
+  `Prezime` VARCHAR(45) NOT NULL,
+  `Plata` INT NOT NULL,
+  `DatumRodjenja` DATE NOT NULL,
+  PRIMARY KEY (`id`));
+
+
+-- -----------------------------------------------------
+-- Table `crossfit`.`Trening`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `crossfit`.`Trening` (
+  `id` INT NOT NULL,
+  `Datum` DATE NOT NULL,
+  `Opis` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`));
+
+
+-- -----------------------------------------------------
+-- Table `crossfit`.`Oprema`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `crossfit`.`Oprema` (
+  `sifra` INT NOT NULL,
+  `Proizvodjac` VARCHAR(45) NOT NULL,
+  `Naziv` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`sifra`));
+
 
 -- -----------------------------------------------------
 -- Table `crossfit`.`Medicinke`
@@ -30,37 +66,6 @@ CREATE TABLE IF NOT EXISTS `crossfit`.`Medicinke` (
     REFERENCES `crossfit`.`Oprema` (`sifra`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
-
-
--- -----------------------------------------------------
--- Table `crossfit`.`Obucavanje`
--- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `crossfit`.`Obucavanje` (
-  `Trener_id` INT NOT NULL,
-  `Trener_id1` INT NOT NULL,
-  PRIMARY KEY (`Trener_id`, `Trener_id1`),  
-  CONSTRAINT `fk_Trener_has_Trener_Trener1`
-    FOREIGN KEY (`Trener_id`)
-    REFERENCES `crossfit`.`Trener` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Trener_has_Trener_Trener2`
-    FOREIGN KEY (`Trener_id1`)
-    REFERENCES `crossfit`.`Trener` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-	
-
--- -----------------------------------------------------
--- Table `crossfit`.`Oprema`
--- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `crossfit`.`Oprema` (
-  `sifra` INT NOT NULL AUTO_INCREMENT,
-  `Proizvodjac` VARCHAR(45) NOT NULL,
-  `Naziv` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`sifra`));
 
 
 -- -----------------------------------------------------
@@ -113,29 +118,45 @@ CREATE TABLE IF NOT EXISTS `crossfit`.`Tegovi` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
-
 -- -----------------------------------------------------
--- Table `crossfit`.`Trener`
--- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `crossfit`.`Trener` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `Ime` VARCHAR(45) NOT NULL,
-  `Prezime` VARCHAR(45) NOT NULL,
-  `Plata` INT NOT NULL,
-  `DatumRodjenja` DATE NOT NULL,
-  PRIMARY KEY (`id`));
-
--- -----------------------------------------------------
--- Table `crossfit`.`Trening`
+-- Table `crossfit`.`Obucavanje`
 -- -----------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `crossfit`.`Trening` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `Datum` DATE NOT NULL,
-  `Opis` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`));
-  
+CREATE TABLE IF NOT EXISTS `crossfit`.`Obucavanje` (
+  `Trener_id` INT NOT NULL,
+  `Trener_id1` INT NOT NULL,
+  PRIMARY KEY (`Trener_id`, `Trener_id1`),  
+  CONSTRAINT `fk_Trener_has_Trener_Trener1`
+    FOREIGN KEY (`Trener_id`)
+    REFERENCES `crossfit`.`Trener` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Trener_has_Trener_Trener2`
+    FOREIGN KEY (`Trener_id1`)
+    REFERENCES `crossfit`.`Trener` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+    
+-- -----------------------------------------------------
+-- Table `crossfit`.`Vodi`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `crossfit`.`Vodi` (
+  `Trener_id` INT NOT NULL,
+  `Trening_id` INT NOT NULL,
+  PRIMARY KEY (`Trener_id`, `Trening_id`),
+  CONSTRAINT `fk_Trener_has_Trening_Trener1`
+    FOREIGN KEY (`Trener_id`)
+    REFERENCES `crossfit`.`Trener` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Trener_has_Trening_Trening1`
+    FOREIGN KEY (`Trening_id`)
+    REFERENCES `crossfit`.`Trening` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+    
+    
 -- -----------------------------------------------------
 -- Table `crossfit`.`Upotreba`
 -- -----------------------------------------------------
@@ -155,23 +176,34 @@ CREATE TABLE IF NOT EXISTS `crossfit`.`Upotreba` (
     REFERENCES `crossfit`.`Oprema` (`sifra`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
-
-
+    
 -- -----------------------------------------------------
--- Table `crossfit`.`Vodi`
--- -----------------------------------------------------
+-- TRIGGERS
+-- -----------------------------------------------------   
+    
+DELIMITER $
 
-CREATE TABLE IF NOT EXISTS `crossfit`.`Vodi` (
-  `Trener_id` INT NOT NULL,
-  `Trening_id` INT NOT NULL,
-  PRIMARY KEY (`Trener_id`, `Trening_id`),
-  CONSTRAINT `fk_Trener_has_Trening_Trener1`
-    FOREIGN KEY (`Trener_id`)
-    REFERENCES `crossfit`.`Trener` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Trener_has_Trening_Trening1`
-    FOREIGN KEY (`Trening_id`)
-    REFERENCES `crossfit`.`Trening` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+CREATE TRIGGER `Prijava_AFTER_INSERT` AFTER INSERT ON `Prijava`
+FOR EACH ROW
+BEGIN
+    UPDATE `Klijent` SET `BrojOdradjenihTreninga` = `BrojOdradjenihTreninga` + 1
+    WHERE new.`Klijent_id`=`id`;
+END;
+$
+
+DELIMITER ;
+
+
+DELIMITER $
+
+CREATE TRIGGER `Vodi_BEFORE_INSERT` BEFORE INSERT ON `Vodi`
+FOR EACH ROW
+BEGIN
+    DECLARE brojPrijavljenih INTEGER;
+    SET brojPrijavljenih = (select count(*) from `Prijava` where `Trening_id` = new.`Trening_id`);         
+    UPDATE `Trener` SET `Plata` = `Plata` + 150 * brojPrijavljenih
+    WHERE `id` = new.`Trener_id`;
+END;
+$
+
+DELIMITER ;
